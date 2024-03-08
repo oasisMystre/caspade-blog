5c2a6e93e6a4e7a2f804c6d4d2ad69341bb2d29a
Include caleb.fun <70612830+oasisMystre@users.noreply.github.com>
2024-03-08 01:01:12 +0100

    bug-fix: fix vercel build error, invalid url, append https:// to domain

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ const poppins = LocalFont({
 });
 
 export const metadata: Metadata = {
-  metadataBase:  new URL(process.env.VERCEL_URL!),
+  metadataBase:  new URL('https://' + process.env.VERCEL_URL!),
   title: "Caspade - Where good ideas find you.",
   description:
     "Caspade is a crypto community where members share ideas, and where expert and undiscovered talents share their skills.",
